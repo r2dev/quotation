0f0f6e8ace1a4dfc0ff579e4234f5a3120786069
k0dede <rren2@myseneca.ca>
2017-10-05 00:33:06 -0400

    fix: quote style update

--- resources/views/quote/edit.blade.php
+++ resources/views/quote/edit.blade.php
@@ -34,10 +34,11 @@
                             <td>{{$product->pivot->height}}</td>
                             <td>{{$product->pivot->lite}}</td>
                             <td>
-                                <form action="{{route('quotes.remove_product_from_quote', ['id' => $quote->id])}}" method="post">
+                                <form action="{{route('quotes.remove_product_from_quote', ['id' => $quote->id])}}"
+                                      method="post">
                                     {{csrf_field()}}
                                     {{method_field('DELETE')}}
-                                    <input type="submit" value="remove" />
+                                    <input type="submit" value="remove"/>
                                     <input type="hidden" value="{{$product->pivot->id}}" name="pq_id">
                                 </form>
                             </td>
@@ -47,59 +48,71 @@
                 </table>
             </div>
             @if ($quote->customer_confirmed == false)
-            <form action="{{route('quotes.add_product', ['id' => $quote->id])}}" method="POST">
-                {{csrf_field()}}
-                <select name="design" id="design">
-                    <option value="" selected disabled hidden>Choose here</option>
+                <form action="{{route('quotes.add_product', ['id' => $quote->id])}}" method="POST">
+                    {{csrf_field()}}
+                    <div class="form-group">
+                        <label for="design">Design</label>
 
-                </select>
-                <select name="style" id="style">
-                    <option value="" selected disabled hidden>Choose here</option>
-                </select>
-                <input type="text" name="quantity" placeholder="quantity" value="1"/>
-                <input type="text" name="width" placeholder="width"/>
-                <input type="text" name="height" placeholder="height"/>
-                <input type="text" name="lite" placeholder="lite"/>
-                <span>Unit price</span><div id="price"> - </div>
-                <span>Total Price</span><div id="total"> - </div>
-                <input type="submit" value="add"/>
-            </form>
+                        <select name="design" id="design" class="form-control">
+                            <option value="" selected disabled hidden>Choose here</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="style">Style</label>
+                        <select name="style" id="style" class="form-control">
+                            <option value="" selected disabled hidden>Choose here</option>
+                        </select>
+                    </div>
+                    <input type="text" name="quantity" placeholder="quantity" value="1"/>
+                    <input type="text" name="width" placeholder="width"/>
+                    <input type="text" name="height" placeholder="height"/>
+                    <input type="text" name="lite" placeholder="lite"/>
+                    <div class="row">
+                        <span>Unit price</span>
+                        <div id="price"> - </div>
+                    </div>
+                    <div class="row">
+                        <span>Total Price</span>
+                        <div id="total"> - </div>
+                    </div>
+                    <input type="submit" value="add"/>
+                </form>
             @endif
 
 
             <form action="{{route('quotes.print_quotation', ['id' => $quote->id])}}" method="POST">
                 {{csrf_field()}}
-                <input type="submit" value="print quotation" />
+                <input type="submit" value="print quotation"/>
             </form>
 
             @if (Auth::user()->permission < 3 && $quote->customer_confirmed == false)
-            <form action="{{route('quotes.client_confirm', ['id' => $quote->id])}}" method="post">
-                {{csrf_field()}}
-                <input type="submit" value="client confirm" />
-            </form>
+                <form action="{{route('quotes.client_confirm', ['id' => $quote->id])}}" method="post">
+                    {{csrf_field()}}
+                    <input type="submit" value="client confirm"/>
+                </form>
             @endif
 
             @if ($quote->customer_confirmed == true && $quote->staff_confirmed == false)
                 <form action="{{route('quotes.client_confirm', ['id' => $quote->id])}}" method="post">
                     {{csrf_field()}}
-                    <input type="submit" value="withdraw quote" />
+                    <input type="submit" value="withdraw quote"/>
                 </form>
             @endif
 
             @if ($quote->staff_confirmed == false && Auth::user()->permission >= 3)
-            <form action="{{route('quotes.production_confirm', ['id' => $quote->id])}}" method="post">
-                {{csrf_field()}}
-                <input type="submit" value="production confirm" />
-            </form>
+                <form action="{{route('quotes.production_confirm', ['id' => $quote->id])}}" method="post">
+                    {{csrf_field()}}
+                    <input type="submit" value="production confirm"/>
+                </form>
             @endif
             @if ($quote->staff_confirmed == true && Auth::user()->permission >= 3)
                 <form action="{{route('quotes.production_confirm', ['id' => $quote->id])}}" method="post">
                     {{csrf_field()}}
-                    <input type="submit" value="print production" />
+                    <input type="submit" value="print production"/>
                 </form>
                 <form action="{{route('quotes.production_confirm', ['id' => $quote->id])}}" method="post">
                     {{csrf_field()}}
-                    <input type="submit" value="print invoice" />
+                    <input type="submit" value="print invoice"/>
                 </form>
             @endif
         </div>
@@ -112,59 +125,29 @@
         window._products = {!! $products_json !!};
         window._styles = {!! $style !!};
         window._styles_format = {}
-        $.map(window._styles, function(value) {
+        $.map(window._styles, function (value) {
             window._styles_format[value.id] = value
         })
-        var priceHandler = function() {
-            var design = parseInt($('#design').find(":selected").data('idx'), 10)
-            var style = parseInt($('#style').val())
-            if (isNaN(design) || isNaN(style)) {
-                $('#price').text(' - ')
-            } else {
-                var result;
-                switch(style) {
-                    case 0:
-                        result = window._products[design].ms;
-                        break;
-                    case 1:
-                        result = window._products[design].mr;
-                        break;
-                    case 2:
-                        result = window._products[design].mp;
-                        break;
-                    case 3:
-                        result = window._products[design].mmdf;
-                        break;
-                    case 4:
-                        result = window._products[design].or;
-                        break;
-                    case 5:
-                        result = window._products[design].mrmdf;
-                        break;
-                    case 6:
-                        result = window._products[design].cr;
-                        break;
-                }
-                $('#price').text(' ' + result + ' ')
-            }
-        }
-        $.each(window._products, function(index, key) {
+        $.each(window._products, function (index, key) {
             $('#design').append($('<option></option>').val(key.id).data('idx', index).text(key.design))
         });
-        $('#design').change(function(e) {
+        $('#design').change(function (e) {
             $('#style').val = ''
             $('#style').empty()
             $('#style').append('<option value="" selected disabled hidden>Choose here</option>')
             var index = parseInt($("#design").find(":selected").data('idx'), 10)
-            $.map(window._products[index].product_styles, function(value, index) {
+            $.map(window._products[index].product_styles, function (value, index) {
                 var style_id = value.style_id
                 if (value.price != 0) {
-                    $('#style').append($('<option></option>').val(style_id).text(window._styles_format[style_id].style))
+                    $('#style').append($('<option></option>').data('price', value.price).val(style_id).text(window._styles_format[style_id].style))
                 }
             })
         })
-//        $('#design, #style').change(function(e) {
-//            priceHandler()
-//        });
+        $("#style").change(function (e) {
+            console.log($("#style").find(":selected").data('price'))
+        })
+        //        $('#design, #style').change(function(e) {
+        //            priceHandler()
+        //        });
     </script>
 @endsection
