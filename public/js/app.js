b81a7e8341dbf0391d31cab54a30c3bae1155139
k0dede <rren2@myseneca.ca>
2017-10-16 17:17:35 -0400

    fix: update product addition form

--- public/js/app.js
+++ public/js/app.js
@@ -66794,9 +66794,9 @@ if (false) {(function () {
   if (!hotAPI.compatible) return
   module.hot.accept()
   if (!module.hot.data) {
-    hotAPI.createRecord("data-v-2b982de0", Component.options)
+    hotAPI.createRecord("data-v-1daeb17a", Component.options)
   } else {
-    hotAPI.reload("data-v-2b982de0", Component.options)
+    hotAPI.reload("data-v-1daeb17a", Component.options)
 ' + '  }
   module.hot.dispose(function (data) {
     disposed = true
@@ -103226,7 +103226,7 @@ module.exports = { render: render, staticRenderFns: staticRenderFns }
 if (false) {
   module.hot.accept()
   if (module.hot.data) {
-    require("vue-hot-reload-api")      .rerender("data-v-2b982de0", module.exports)
+    require("vue-hot-reload-api")      .rerender("data-v-1daeb17a", module.exports)
   }
 }
 
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,66 +1,9 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
-            <table class="table">
-                <thead>
-                <tr>
-                    <th>
-                        Design Name
-                    </th>
-                    <th>
-                        Maple Select
-                    </th>
-                    <th>
-                        Maple Regular
-                    </th>
-                    <th>
-                        Maple Paint
-                    </th>
-                    <th>
-                        Maple MDF
-                    </th>
-                    <th>
-                        Oak Regular
-                    </th>
-                    <th>
-                        Maple Regular MDF
-                    </th>
-                    <th>
-                        Cherry Regular
-                    </th>
-                    <th>
-                        action
-                    </th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach ($products as $product)
-                    <tr>
-                        <td>{{$product->design}}</td>
-                        <td>{{$product->price_0}}</td>
-                        <td>{{$product->price_1}}</td>
-                        <td>{{$product->price_2}}</td>
-                        <td>{{$product->price_3}}</td>
-                        <td>{{$product->price_4}}</td>
-                        <td>{{$product->price_5}}</td>
-                        <td>{{$product->price_6}}</td>
-                        <td>
-                            <form action="{{route('products.destroy', ['id' => $product->id])}}" method="POST">
-                                <input type="hidden" name="_method" value="DELETE">
-                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                <input type="submit" value="delete"/>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-
-                </tbody>
-            </table>
-            {!! $products->appends(['limit' => $limit])->links() !!}
-            @if (Auth::user()->permission >= 3)
-            <form action="{{route('products.store')}}" method="POST">
-                <table class="table  table-responsive">
+            <div class="table-responsive">
+                <table class="table">
                     <thead>
                     <tr>
                         <th>
@@ -87,30 +30,85 @@
                         <th>
                             Cherry Regular
                         </th>
-                        <th>
-                            action
-                        </th>
+                        @if (Auth::user()->permission >= 3)
+                            <th>
+                                action
+                            </th>
+                        @endif
                     </tr>
                     </thead>
                     <tbody>
-                    <tr>
+                    @foreach ($products as $product)
+                        <tr>
+                            <td>{{$product->design}}</td>
+                            <td>{{number_format($product->price_0, 2)}}</td>
+                            <td>{{number_format($product->price_1, 2)}}</td>
+                            <td>{{number_format($product->price_2, 2)}}</td>
+                            <td>{{number_format($product->price_3, 2)}}</td>
+                            <td>{{number_format($product->price_4, 2)}}</td>
+                            <td>{{number_format($product->price_5, 2)}}</td>
+                            <td>{{number_format($product->price_6, 2)}}</td>
+                            @if (Auth::user()->permission >= 3)
 
-                        <td><input name="design" type="text">{{csrf_field()}}</td>
-                        <td><input name="price[0]" type="text"></td>
-                        <td><input name="price[1]" type="text"></td>
-                        <td><input name="price[2]" type="text"></td>
-                        <td><input name="price[3]" type="text"></td>
-                        <td><input name="price[4]" type="text"></td>
-                        <td><input name="price[5]" type="text"></td>
-                        <td><input name="price[6]" type="text"></td>
-                        <td>
-                            <input type="submit" value="submit">
-                        </td>
-                    </tr>
+                                <td>
+                                    <form action="{{route('products.destroy', ['id' => $product->id])}}" method="POST">
+                                        <input type="hidden" name="_method" value="DELETE">
+                                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                        <input type="submit" value="delete"/>
+                                    </form>
+                                </td>
+                            @endif
+                        </tr>
+                    @endforeach
+
                     </tbody>
                 </table>
-            </form>
-                @endif
+            </div>
+            {!! $products->appends(['limit' => $limit])->links() !!}
+            <div class="row">
+                <div class="col-sm-12 col">
+                    @if (Auth::user()->permission >= 3)
+                        <form action="{{route('products.store')}}" method="POST">
+
+                            <div class="form-group">
+                                <label for="design_name">Design Name</label>
+                                <input type="text" name="design" id="design_name" class="form-control">
+                                {{csrf_field()}}
+                            </div>
+                            <div class="form-group">
+                                <label for="maple_select">Maple Select</label>
+                                <input type="text" name="price[0]" id="maple_select" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="maple_regular">Maple Regular</label>
+                                <input type="text" name="price[1]" id="maple_regular" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="maple_paint">Maple Paint</label>
+                                <input type="text" name="price[2]" id="maple_paint" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="maple_mdf">Maple MDF</label>
+                                <input type="text" name="price[3]" id="maple_mdf" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="oak_regular">Oak Regular</label>
+                                <input type="text" name="price[4]" id="oak_regular" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="maple_regular_mdf">Maple Regular MDF</label>
+                                <input type="text" name="price[5]" id="maple_regular_mdf" class="form-control">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="cherry_regular">Cherry Regular</label>
+                                <input type="text" name="price[6]" id="cherry_regular" class="form-control">
+                            </div>
+                            <button class="btn btn-default">Submit</button>
+                        </form>
+                    @endif
+                </div>
+            </div>
         </div>
 
 
