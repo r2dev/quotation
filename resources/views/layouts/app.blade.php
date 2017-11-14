62f1d8c8610a7d2d8525c96ef2c3da0f96e93e68
k0dede <rren2@myseneca.ca>
2017-11-14 17:50:31 -0500

    fix:  layout style change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -105,7 +105,7 @@
                     </ul>
 
                 </div>
-                <div class="col-sm-9 col-smoffset-3 col-md-10 col-md-offset-2 main">
+                <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
                     @yield('content')
                 </div>
             @else
